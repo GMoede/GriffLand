4aa83882fc20d13a41b521620356b29ed3170b0a
GMoede <griffinmoede@gmail.com>
2024-03-01 16:51:19 -0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # GriffLand
 My own personal maze and website.
+
+Ideas:
+
+Website starts by fading into looking through website through eyes, eyes change and show room .guy blinking looking around and is chased by a bunch of mice. . The mice tracks your mouse and runs after you. If it catches you, game over retry. If you click, you shoot and it makes real gun sound and muzzle flash. Make sure muzzle flash shows actual lighting. 
---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import lauren from './lauren.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div class='x'>
+          <img src={lauren} class='x y' className="App-logo" alt="logo codepen" />
+        </div>
+        
+        <p>
+          Lauren <code>src/App.js</code> Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren Lauren 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
